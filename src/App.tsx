ba2c0091cf7f65123ec4812b8890c26c425f8f9e
Carlton Joseph <carlton.joseph@gmail.com>
2025-07-31 10:26:23 -0700

    add more details

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,13 @@ function App() {
         <table className="pt-2 border-collapse border border-blue-800">
           <tbody>
             <tr>
-              <td className="border border-blue-400 px-2 py-1">Vyonne</td>
               <td className="border border-blue-400 px-2 py-1">Christian</td>
-              <td className="border border-blue-400 px-2 py-1">Bruce</td>
+              <td className="border border-blue-400 px-2 py-1">Vyonne</td>
+              <td className="border border-blue-400 px-2 py-1">Deb</td>
+              <td className="border border-blue-400 px-2 py-1">Jackie</td>
             </tr>
             <tr>
+              <td className="border border-blue-400 px-2 py-1">Bruce</td>
               <td className="border border-blue-400 px-2 py-1">Teresa</td>
               <td className="border border-blue-400 px-2 py-1">Sandy</td>
               <td className="border border-blue-400 px-2 py-1">Carlton</td>
@@ -36,18 +38,29 @@ function App() {
           <tbody>
             <tr>
               <td className="border border-blue-400 px-2 py-1">
-                1<sup>st</sup> Friday
+                Friday<br/>August 1<sup>st</sup>
               </td>
               <td className="border border-blue-400 px-2 py-1">
-                Drive to Montrose
+                <ul>
+                  <li>Drive to Montrose</li>
+                  <li>Japanese Theme Dinner</li>
+                </ul>
               </td>
             </tr>
             <tr>
               <td className="border border-blue-400 px-2 py-1">
-                2<sup>nd</sup> Saturday
+                Saturday<br/>August 2<sup>nd</sup>
               </td>
               <td className="border border-blue-400 px-2 py-1">
-                Telluride Via Ferrata &amp; Hike
+                <ul>
+                  <li>
+                    <a href="https://ourayviaferrata.org/" className="underline text-blue-400">
+                      Ouray Via Ferratas
+                    </a>
+                  </li>
+                  <li>Upstream (VF4.5B)</li>
+                  <li>Downstream (VF4B optional VF5)</li>
+                </ul>
               </td>
             </tr>
             <tr>
@@ -55,7 +68,21 @@ function App() {
                 3<sup>rd</sup> Sunday
               </td>
               <td className="border border-blue-400 px-2 py-1">
-                Ouray Via Ferrata x2
+                <ul>
+                  <li>Ouray Canyons</li>
+                  <li>
+                    <a href="https://ropewiki.com/index.php?title=Angel_Creek_(Lower)" className="underline text-blue-400">
+                    Angle Creek 
+                    </a>
+                    {" "}4 raps, 45ft
+                  </li>
+                  <li>
+                    <a href="https://ropewikiacom/index.php?title=Angel_Creek_(Lower)" className="underline text-blue-400">
+                    Portland Creek
+                    </a>
+                    {" "}2-3 raps, 55ft
+                  </li>
+                </ul>
               </td>
             </tr>
             <tr>
@@ -63,32 +90,40 @@ function App() {
                 4<sup>th</sup> Monday
               </td>
               <td className="border border-blue-400 px-2 py-1">
-                <p>Easy Class C Canyon tbd</p>
-                <p>Drive to Las Vegas</p>
+                <ul>
+                  <li>
+                    <a href="https://www.telluride.com/activity/telluride-via-ferrata/" className="underline text-blue-400">
+                      Telluride Via Ferrata
+                    </a>
+                  </li>
+                  <li>
+                    Drive to Las Vegas
+                  </li>
+                </ul>
               </td>
             </tr>
           </tbody>
         </table>
-        <h2 className="font-semibold mb-2 mt-3 text-xl">
-          Tentative Lodging Cost
-        </h2>
-        <p>Per Person For 3 nights</p>
-        <p>
-          <a href="https://t.vrbo.io/9kGQCpFYHUb" className="underline">
-            {703 / 4}
-          </a>
-          ,{" "}
-          <a href="https://t.vrbo.io/SF8IcTfZHUb" className="underline">
-            {640 / 4}
-          </a>
-          ,{" "}
-          <a
-            href="https://www.airbnb.com/rooms/1391401004733549599?check_in=2025-08-01&check_out=2025-08-04&guests=6&adults=4&s=67&unique_share_id=5341ded9-3c6e-474f-90ed-3f9871b982fc"
-            className="underline"
-          >
-            {610 / 4}
-          </a>
-        </p>
+        {/* <h2 className="font-semibold mb-2 mt-3 text-xl"> */}
+        {/*   Tentative Lodging Cost */}
+        {/* </h2> */}
+        {/* <p>Per Person For 3 nights</p> */}
+        {/* <p> */}
+        {/*   <a href="https://t.vrbo.io/9kGQCpFYHUb" className="underline"> */}
+        {/*     {703 / 4} */}
+        {/*   </a> */}
+        {/*   ,{" "} */}
+        {/*   <a href="https://t.vrbo.io/SF8IcTfZHUb" className="underline"> */}
+        {/*     {640 / 4} */}
+        {/*   </a> */}
+        {/*   ,{" "} */}
+        {/*   <a */}
+        {/*     href="https://www.airbnb.com/rooms/1391401004733549599?check_in=2025-08-01&check_out=2025-08-04&guests=6&adults=4&s=67&unique_share_id=5341ded9-3c6e-474f-90ed-3f9871b982fc" */}
+        {/*     className="underline" */}
+        {/*   > */}
+        {/*     {610 / 4} */}
+        {/*   </a> */}
+        {/* </p> */}
       </div>
       <div className="bg-blue-200 opacity-90 h-full"></div>
     </>
